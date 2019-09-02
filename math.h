b35a75ec59f15f89eeb3fccd77f8d5b24d238d0f
luisenriqueagui <54040432+luisenriqueagui@users.noreply.github.com>
2019-09-02 00:19:04 -0400

    Create math.h

---
+++ math.h
@@ -0,0 +1,19 @@
+# fundamentos-programacion
+//raiz
+
+
+#include <iostream>
+#include "math.h"
+
+using namespace std;
+int main()
+{
+	float x,y,resultado;
+	cout<<"ingrese el valor x: "; cin>>x;
+	cout<<"ingrese el valor y: "; cin>>y;
+	resultado= (sqrt(x))/(pow(y,2)-1);
+	cout<<"el resultado es"
+	
+
+	return 0;
+}
